2ba698b2eaa3926411574fc3dd1f0afbeae4be14
Shohei Shibata <shohei.shibata.dev@gmail.com>
2023-03-31 08:07:07 -0400

    add gh-pages

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,8 +4,9 @@
 module.exports = {
   siteMetadata: {
     title: `Simple Weather`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://shohei-shibata.github.io/weather/`
   },
+  pathPrefix: "/weather",
   plugins: ["gatsby-plugin-image", "gatsby-plugin-sitemap", {
     resolve: 'gatsby-plugin-manifest',
     options: {
